fa18f06c1f08a90c16c2de31977fc8280e88e9cc
Ridwan <aladebaruwa@gmail.com>
2023-09-09 20:06:05 +0100

    add

--- index.js
+++ index.js
@@ -62,5 +62,7 @@ window.onload = init;
 function addSongs() {
     let song1 = document.getElementById("song1");
     let song2 = document.getElementById("song2");
-    let song3 = document.getElementById("song3")
+    let song3 = document.getElementById("song3");
+    song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+    song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
 }
