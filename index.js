16ad0a3413affec48d59b367de685e3049548808
Ridwan <aladebaruwa@gmail.com>
2023-09-10 15:46:23 +0100

    add

--- index.js
+++ index.js
@@ -73,8 +73,11 @@ function showTemp() {
     let tempByHour= [59.2, 60.1, 63, 65, 62];
     for ( i = 0; i < 5; i++ ) {
         let theTemp = tempByHour[i];
-        let id = theTemp + i;
+        let id = "temp" + i;
         let li = document.getElementById(id);
+        if (i==1) {
+            li.innerHTML= "The temperature at noon was " + theTemp;
+        }
     }
 }
 window.onload = showTemp;
