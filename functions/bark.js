33e49772f92729d77f9a5cab9b703825715e8337
Ridwan <aladebaruwa@gmail.com>
2023-09-17 13:02:00 +0100

    add

--- functions/bark.js
+++ functions/bark.js
@@ -1,6 +1,7 @@
 window.onload = bark;
 function bark(dogName, dogWeight) {
     let name = document.getElementById("dogName");
+    let dogName = name.ariaValueMax;
     if (dogWeight < 50) {
         return dogName + "Yip";
     }
