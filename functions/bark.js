8e904a505658300297d88477d77e108fd5f337ad
Ridwan <aladebaruwa@gmail.com>
2023-09-17 13:03:10 +0100

    add

--- functions/bark.js
+++ functions/bark.js
@@ -3,7 +3,7 @@ function bark(dogName, dogWeight) {
     let name = document.getElementById("dogName");
     let dogName = name.value;
     let weight = document.getElementById("dogWeight");
-    
+    let dogWeight = weight.value;
     if (dogWeight < 50) {
         return dogName + "Yip";
     }
