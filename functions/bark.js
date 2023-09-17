831a6b837919080bfe2d3747bc343d3396d4a3f2
Ridwan <aladebaruwa@gmail.com>
2023-09-17 13:14:31 +0100

    add

--- functions/bark.js
+++ functions/bark.js
@@ -4,13 +4,6 @@ function init() {
     button.onclick = bark;
 }
 function bark(dogName, dogWeight) {
-    let name = document.getElementById("dogName");
-    let dogName = name.value;
-    let weight = document.getElementById("dogWeight");
-    let dogWeight = weight.value;
-    let p = document.createElement("p");
-    let ul = document.getElementById("message");
-    ul.appendChild(p)
     if (dogWeight < 50) {
         p.innerHTML = dogName + "Yips";
     }
