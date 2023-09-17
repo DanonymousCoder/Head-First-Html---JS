507b5d839eeb05205fa573adc9ea5caf19e37f77
Ridwan <aladebaruwa@gmail.com>
2023-09-17 12:17:30 +0100

    add

--- Webville Tunes/playlist_store.js
+++ Webville Tunes/playlist_store.js
@@ -22,4 +22,8 @@ function getStoreArray(key) {
     if (playlistArray == null || playlistArray == " ") {
         playlistArray = new Array();
     }
+    else {
+        playlistArray = JSON.parse(playlistArray);
+    }
+    return playlistArray
 }
