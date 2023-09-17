ffd45e0d5c3d57e6920e4a34891edf3cb070607b
Ridwan <aladebaruwa@gmail.com>
2023-09-17 13:11:40 +0100

    add

--- Webville Tunes/playlist.js
+++ Webville Tunes/playlist.js
@@ -10,7 +10,7 @@ function handleButtonClick() {
     let songName = textInput.value;
     let li = document.createElement("li");
    li.innerHTML = songName;
-   let ul = document.getElementById("list");
+   let ul = document.getElementById("playlist");
    ul.appendChild(li);
    save(songName);
 }
