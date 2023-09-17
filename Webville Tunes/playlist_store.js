1a1722a8f09474ff757155922938c3ad88824fa7
Ridwan <aladebaruwa@gmail.com>
2023-09-17 12:29:03 +0100

    add

--- Webville Tunes/playlist_store.js
+++ Webville Tunes/playlist_store.js
@@ -1,3 +1,18 @@
+
+    function init() {
+        var button = document.getElementById("addButton");
+        button.onclick = handleButtonClick;
+        loadPlaylist();
+       } 
+       function handleButtonClick() {
+        var textInput = document.getElementById("songTextInput");
+        var songName = textInput.value;
+        var li = document.createElement("li");
+        li.innerHTML = songName;
+        var ul = document.getElementById("list");
+        ul.appendChild(li);
+        save(songName)
+       }
 function save(item) {
     let playlistArray = getStoreArray("playlist");
     playlistArray.push(item);
