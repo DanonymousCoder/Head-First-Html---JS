10942ce99f559273cb67b9ebef9764fab7f165ea
Ridwan <aladebaruwa@gmail.com>
2023-10-08 13:09:05 +0100

    add

--- functions/bark.js
+++ functions/bark.js
@@ -96,6 +96,8 @@ function loseWeight(dog) {
 alert(fido.name + " now weighs " + fido.weight + " " + "pounds");
 
 
+let fido = new Dog("Tiny", "Mixed", 38);
+
 let movie1 = {
     title : "Plan 9 from Outer Space",
     genre : "Cult classic",
